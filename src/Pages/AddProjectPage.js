a137e5c53eeb46197759ab8c26de88435f74c08a
Tamar Sapir <Tamarsapir05@gmail.com>
2025-06-18 23:13:55 +0300

    little fix

--- src/Pages/AddProjectPage.js
+++ src/Pages/AddProjectPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './AddProjectModal.css';
+import './AddProjectPage.css';
 import axios from 'axios';
 
 function AddProjectModal({ show, onClose, onProjectAdded }) {
