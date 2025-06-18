bf452f1dc4d1d19897cb1a0df545a5c0fbb66141
Tamar Sapir <Tamarsapir05@gmail.com>
2025-06-18 23:11:48 +0300

    little fix

--- src/Pages/ManageProjectsPage.js
+++ src/Pages/ManageProjectsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './ManageProjectsPage.css';
+import './AddProjectPage.css';
 import axios from 'axios';
 import AddProjectPage from './AddProjectPage';
 
