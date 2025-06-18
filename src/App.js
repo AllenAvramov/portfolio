af9f3cc5901d18bf6e922e3f730215740e6f88a3
Tamar Sapir <Tamarsapir05@gmail.com>
2025-06-18 22:49:06 +0300

    move useLocation into component within Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import PortfolioPage from './Pages/PortfolioPage';
 import AdminPage from './Pages/AdminPage';
 import AboutPage from './Pages/AboutPage';
@@ -15,9 +16,8 @@ import ProtectedRoute from './Components/Guards/ProtectedRoute';
 import ManageProjectsPage from './Pages/ManageProjectsPage';
 import ViewMessagesPage from './Pages/ViewMessagesPage';
 import AdminSettingsPage from './Pages/AdminSettingsPage';
-import { useLocation } from 'react-router-dom';
 
-function App() {
+function AppRoutes() {
   const location = useLocation();
   const isMaintenance = localStorage.getItem('maintenanceMode') === 'true';
 
@@ -45,21 +45,27 @@ function App() {
   }
 
   return (
-    <Router>
-      <NavBar></NavBar>
+    <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<PortfolioPage />} />
-        <Route path="/admin" element={<AdminPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/skills" element={<SkillsPage />} />
         <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin" element={<ProtectedRoute><AdminPage /></ProtectedRoute>}/>
-        <Route path="/admin/projects"   element={<ProtectedRoute><ManageProjectsPage /></ProtectedRoute>} />
+        <Route path="/admin" element={<ProtectedRoute><AdminPage /></ProtectedRoute>} />
+        <Route path="/admin/projects" element={<ProtectedRoute><ManageProjectsPage /></ProtectedRoute>} />
         <Route path="/admin/messages" element={<ProtectedRoute><ViewMessagesPage /></ProtectedRoute>} />
         <Route path="/admin/settings" element={<ProtectedRoute><AdminSettingsPage /></ProtectedRoute>} />
-        
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppRoutes />
     </Router>
   );
 }
